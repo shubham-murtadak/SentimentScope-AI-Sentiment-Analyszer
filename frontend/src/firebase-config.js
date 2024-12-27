b5dc8a4f9a64a06c884f86504a954933d7faa68a
Shubham Murtadak <125735421+shubham-murtadak@users.noreply.github.com>
2024-12-28 00:01:55 +0530

    Update firebase-config.js

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -1,22 +1,35 @@
-// src/firebase-config.js
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
+// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBtqJqStm6rFspxQPvzuIaX1XiMmyJ_Tcg",
-    authDomain: "sentiment-analysis-6b019.firebaseapp.com",
-    projectId: "sentiment-analysis-6b019",
-    storageBucket: "sentiment-analysis-6b019.firebasestorage.app",
-    messagingSenderId: "213934842335",
-    appId: "1:213934842335:web:1cb2984c85526105a6e46a",
-    measurementId: "G-1DJWYSDZED"
-  };
-
+  apiKey: "AIzaSyBP-Z0NNQEQ3zlZKtA0h0ffORSR11cnhMg",
+  authDomain: "anuwad-d169a.firebaseapp.com",
+  projectId: "anuwad-d169a",
+  storageBucket: "anuwad-d169a.firebasestorage.app",
+  messagingSenderId: "491992642923",
+  appId: "1:491992642923:web:75904b4310a19c7873082e",
+  measurementId: "G-69JECWRC4X"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged };
+export {
+  auth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+};
