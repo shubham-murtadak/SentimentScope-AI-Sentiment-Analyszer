3b47455c510f7e4d5293bb395023aed80dcf0e05
Shubham Murtadak <125735421+shubham-murtadak@users.noreply.github.com>
2024-12-28 15:19:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -97,7 +97,7 @@ function App() {
       <nav className="navbar">
         <div className="navbar-logo">SentimentScope 🚀</div>
         <ul className="navbar-links">
-          <li><a href="#" onClick={handleGoBack}>Home</a></li>
+          <li><a onClick={handleGoBack}>Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#contact">Contact Us</a></li>
           {user ? (
